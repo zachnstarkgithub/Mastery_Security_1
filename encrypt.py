a2a1882135707d6a674baf6b380ab60e77449740
zachnstarkgithub <zachnstark@gmail.com>
2016-11-19 04:27:02 -0800

    added comments

--- encrypt.py
+++ encrypt.py
@@ -5,14 +5,23 @@ raw_text = sys.argv[1]
 print "raw_text",raw_text
 
 encoded_text = []
+
+#encryption scheme
 for char in raw_text:
 	encoded_text.append(chr(ord(char) + 4))
+
+#turns character array into a string
 encoded_text = ''.join(encoded_text)
 
 decoded_text = []
+
+#decryption scheme
 for char in encoded_text:
 	decoded_text.append(chr(ord(char) - 4))
+
+#turns character array into a string
 decoded_text = ''.join(decoded_text)
 
+#print everything
 print "encoded_text", encoded_text
 print "decoded_text", decoded_text
